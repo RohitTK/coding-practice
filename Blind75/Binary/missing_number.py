7b16f4917026f7b3650a679942f12275be68854a
RohitTK <95155191+RohitTK@users.noreply.github.com>
2022-07-29 00:46:14 -0400

    Missing Number

---
+++ Blind75/Binary/missing_number.py
@@ -0,0 +1,47 @@
+from typing import List
+
+"""Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is
+missing from the array.
+
+
+
+Example 1:
+
+Input: nums = [3,0,1] Output: 2 Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,
+3]. 2 is the missing number in the range since it does not appear in nums. Example 2:
+
+Input: nums = [0,1] Output: 2 Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,
+2]. 2 is the missing number in the range since it does not appear in nums. Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1] Output: 8 Explanation: n = 9 since there are 9 numbers, so all numbers are in the
+range [0,9]. 8 is the missing number in the range since it does not appear in nums. """
+
+
+class Solution:
+    # Logic:
+    def missingNumber1(self, nums: List[int]) -> int:
+        ct = 0
+        for i in sorted(nums):
+            if i != ct:
+                return ct
+            ct += 1
+        return ct
+
+    # Logic: Maths formula :p
+    # n * (n+1) / 2: Sum of first n numbers
+    def missingNumber2(self, nums:List[int]) -> int:
+        n = len(nums)
+        s = sum(nums)
+        return ((n * (n + 1))//2) - s
+
+
+testcases = [[3, 0, 1],
+             [0, 1],
+             [9, 6, 4, 2, 3, 5, 7, 0, 1]]
+
+sol = Solution()
+
+for testcase in testcases:
+    print('Input:', testcase,
+          '\nOutput 1:', sol.missingNumber1(testcase),
+          '\nOutput 2:', sol.missingNumber2(testcase), '\n')
